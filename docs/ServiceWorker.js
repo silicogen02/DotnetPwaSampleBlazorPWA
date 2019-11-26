8c73d7eea794536ab7fc1dce3dc9ff423b48af24
Alexey Chuprov <alexey.chuprov@gmail.com>
2019-11-26 05:44:00 +0300

    update 2019.11.26 05:43:57

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,5 +1,5 @@
-const baseURL = './';
-const indexURL = './index.html';
+const baseURL = '/';
+const indexURL = '/index.html';
 const networkFetchEvent = 'fetch';
 const swInstallEvent = 'install';
 const swInstalledEvent = 'installed';
@@ -7,7 +7,7 @@ const swActivateEvent = 'activate';
 const staticCachePrefix = 'blazor-cache-v';
 const staticCacheName = 'blazor-cache-v1';
 const requiredFiles = [
-"./_framework/blazor.boot.json",
+"/_framework/blazor.boot.json",
 "/_framework/blazor.server.js",
 "/_framework/blazor.webassembly.js",
 "/_framework/wasm/mono.js",
